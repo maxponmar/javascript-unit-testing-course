9aea2c97b9470bd9a4a16c021920e3c51d36663e
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-06-02 05:41:32 -0700

    Section 6: Concurrent tests.

--- testing-hooks/hooks/hooks.test.js
+++ testing-hooks/hooks/hooks.test.js
@@ -1,4 +1,12 @@
-import { it, expect, beforeAll, beforeEach, afterEach, afterAll } from "vitest";
+import {
+  it,
+  expect,
+  beforeAll,
+  beforeEach,
+  afterEach,
+  afterAll,
+  describe,
+} from "vitest";
 
 import { User } from "./hooks";
 
@@ -21,29 +29,43 @@ afterAll(() => {
   console.log("afterAll()");
 });
 
-it("should update the email", () => {
+// You can use .concurrent and all test inside will be concurrent
+// describe.concurrent()
+
+it.concurrent("should update the email", () => {
   const newTestEmail = "test2@test.com";
   user.updateEmail(newTestEmail);
 
   expect(user.email).toBe(newTestEmail);
 });
 
-it("should have an email property", () => {
+it.concurrent("should have an email property", () => {
   expect(user).toHaveProperty("email");
 });
 
-it("should store the provided email value", () => {
+it.concurrent("should store the provided email value", () => {
   expect(user.email).toBe(testEmail);
 });
 
-it("should clear the email", () => {
+it.concurrent("should clear the email", () => {
   user.clearEmail();
 
   expect(user.email).toBe("");
 });
 
-it("should still have an email property after clearing the email", () => {
-  user.clearEmail();
+it.concurrent(
+  "should still have an email property after clearing the email",
+  () => {
+    user.clearEmail();
 
-  expect(user).toHaveProperty("email");
-});
+    expect(user).toHaveProperty("email");
+  }
+);
+
+/*
+Even when not adding the .concurrent property / annotation, tests that are stored in different files are executed concurrently (i.e., in parallel). This is done by both Vitest and Jest - ensuring that your tests run in a short amount of time.
+
+With .concurrent you can enforce this behavior also inside the individual files (i.e., tests that live in one and the same file are executed concurrently).
+
+Concurrent execution can reduce the amount of time your tests need to execute. A downside of concurrent execution is, that tests that perform clashing (global) state manipulations may interfere with each other.
+*/
