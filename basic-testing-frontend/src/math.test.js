47651e11689d69a5556c8a33d2a1581f71d55f4c
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-06-01 19:18:03 -0700

    Section 3: More tests.

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -12,3 +12,29 @@ it("Should summarize all number values in an array", () => {
   const expectedResult = numbers.reduce((acum, current) => acum + current, 0);
   expect(result).toBe(expectedResult);
 });
+
+it("Should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("Should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (acumulate, currrent) => +acumulate + +currrent,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it("Should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+  const result = add(numbers);
+  expect(result).toBe(0);
+});
