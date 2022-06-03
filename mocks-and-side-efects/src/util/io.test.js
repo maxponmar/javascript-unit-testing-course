dbbfaff2866421405c3f931499fc1372370f41c5
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-06-03 03:39:09 -0700

    Section 7: Mocks and automocking.

--- mocks-and-side-efects/src/util/io.test.js
+++ mocks-and-side-efects/src/util/io.test.js
@@ -1,11 +1,18 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 
 import writeData from "./io";
+
+vi.mock("fs");
 
 it("Should execute the writeFile method", () => {
   const testData = "Test";
   const testFileName = "test.txt";
   // This test has a side effect, it writes something to the file system
   // side effect => interacting with some other system
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+
+  writeData(testData, testFileName);
+
+  expect(fs.writeFile).toBeCalled();
 });
