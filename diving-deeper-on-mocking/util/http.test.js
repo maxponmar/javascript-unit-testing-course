f2fc2d85f4146863774924dd17cb010194793a09
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-06-04 05:32:29 -0700

    Section 8: Test specific mocking logic.

--- diving-deeper-on-mocking/util/http.test.js
+++ diving-deeper-on-mocking/util/http.test.js
@@ -6,6 +6,9 @@ const testResponseData = { testKey: "testData" };
 
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not a string (Valid JSON object)");
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -24,4 +27,17 @@ it("Should return any available response data", () => {
   const testData = { key: "test" };
 
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it("Should convert the provided data to JSON before sending the request", async () => {
+  const testData = { key: "test" };
+
+  let errorMessage;
+  try {
+    await sendDataRequest(testData);
+  } catch (error) {
+    errorMessage = error.message;
+  }
+
+  expect(errorMessage).not.toBe("Not a string (Valid JSON object)");
 });
